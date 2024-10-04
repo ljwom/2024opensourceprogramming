2c1f436b9c845bfb857810ccb838e56209b3863d
ljwom <ljwomlj@gmail.com>
2024-10-04 12:01:16 +0900

    종원

--- week05/main.go
+++ week05/main.go
@@ -1,10 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
+
 	var i int = 55
+	fmt.Println(math.Ceil(3.99))
+
 	fmt.Printf("value i : %d\n", i)
 	fmt.Print("value i : ", i, "\n")
-	fmt.Print("value i :", i)
+	fmt.Print("value i : ", i)
 }
--- week06/main.go
+++ week06/main.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
-	i := 13
-	var f float64 = 12.9
-	c1 := 'A'
-	c2 := '2'
-
-	fmt.Printf("value i : %d, value f : %f\n", i, f)
-	fmt.Printf("%d * %f = %d\n", i, f, i*int(f))
-	fmt.Println(c1, c2)
-	fmt.Println(reflect.TypeOf(i), reflect.TypeOf(c1), reflect.TypeOf(c2))
+	var f float64
+	var i int
+	var b bool
+	var s string
+	fmt.Println(f, i, b, s)
+	fmt.Printf("%f%t%s%d", f, b, s, i)
+	f = 2.7
+	i = 3
+	fmt.Print("\n\n", f > float64(i), "\n")
 }
