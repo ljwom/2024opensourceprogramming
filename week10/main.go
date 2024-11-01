1793c742a1ca226a6f792c02867d82e10ec70eee
sky1023 <cvx3449@gmail.com>
2024-11-01 11:07:16 +0900

    10week

---
+++ week10/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Print("정수 입력 : ")
+	r := bufio.NewReader(os.Stdin)
+	i, err := r.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	i = strings.TrimSpace(i)
+	n, err := strconv.Atoi(i)
+
+	var isPrime bool = true
+
+	if n < 2 {
+		isPrime = false
+	} else {
+		for j := 2; j < n; j++ {
+			if n%j == 0 {
+				isPrime = false
+				break // 무의미한 반복을 제거
+			}
+			fmt.Printf("%d ", j) // 반복문 횟수 확인용 코드
+		}
+	}
+	if isPrime {
+		fmt.Printf("%d는(은) 소수입니다", n)
+	} else {
+		fmt.Printf("%d는(은) 소수가 아닙니다", n)
+	}
+}
--- week9/main.go
+++ week9/main.go
@@ -2,14 +2,15 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 )
 
 func main() {
-	target := rand.Intn(5) + 1
-	fmt.Printf("%d\n", target)
-}
-
-
-
-
+	var result string
+	fmt.Sprintf("%0.1f 나누기 %0.1f는 %0.2f입니다\n", 1.0, 3.0, 1.0/3.0)
+	fmt.Print(result)
+     }
+    i := 1
+	for i < 30{
+	fmt.Printf("%5s\n", i)
+	i++
+	};
